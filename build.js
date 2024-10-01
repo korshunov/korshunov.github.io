82802b45eb4b053bfbebb84867c5801aa185f9b8
Fedor Korshunov <mail@fedor.cc>
2024-10-01 20:41:31 +0800

    Add posts

--- build.js
+++ build.js
@@ -15,7 +15,7 @@ const postTemplate = readTextFile(`${rootDir}/posts/template.html`)
 const posts = JSON.parse(readTextFile(`${rootDir}/posts/pages.json`))
 
 // Build root-level pages
-;['index', 'now', 'work'].forEach(
+;['index', 'now', 'work', 'posts'].forEach(
 	name => {
 		fs.writeFileSync(
 			`${__dirname}/build/${name}.html`,
